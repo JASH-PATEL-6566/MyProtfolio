1fa058fbea4231d356127bd1ac6d5285352c1009
JASH-PATEL-6566 <jashpatel6566@gmail.com>
2024-06-16 03:05:33 +0530

    fix bug in AboutHome

--- src/Components/AboutHome/AboutHome.js
+++ src/Components/AboutHome/AboutHome.js
@@ -11,10 +11,10 @@ function AboutHome() {
             <Title symbol={"/"} title={"about_me"} discription={"ajsda aksjd aksdh kasdhkahsdk haksd  aksjdh akjdhak jdshkaj dshk asd"} />
             <div className="d-flex flex-column justify-content-center align-items-center">
                 <Row>
-                    <Column className="column-border text-decoration-underline mid_fonts">
+                    <Column className="align-items-center column-border text-decoration-underline mid_fonts">
                         <p className='w-75 py-5'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae consequuntur fugiat ullam quia, voluptatem explicabo ad corporis aperiam? Quae, ea.</p>
                     </Column>
-                    <Column className="small_fonts">
+                    <Column className="align-items-center small_fonts">
                         <p className='w-75 py-5 yellow_color' style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum distinctio velit dolore consequuntur sapiente quas ipsa aut consequatur doloribus fugiat similique, sunt enim mollitia quam, nemo earum illum doloremque </p>
                     </Column>
                 </Row>
